9e795823ee5bc8c4845cb8cf4d655d12d9c3b53a
alex_mech <samik.mechanic@gmail.com>
2014-03-12 00:52:20 +0400

    [!] proto_old.h старые прототипы, для меня оставил [!] в командах переделаны обращения к регистрам (сами регистры все еще фиктивные, как и константа immediate, кроме HI и LO) [+] добавлены некоторые действия для дальнейшей обработки переполнений [+] добавлены branch'и и jump'ы, пока по аналогии с реальной работой MIPSa, а дальше посмотрим

--- commands/commands_old.cpp
+++ commands/commands_old.cpp
@@ -4,7 +4,7 @@ using namespace std;
 //==================================================
 //commands ADD and its derivatives
 //--------------------------------------------------
-void ADD (const Core (&o_core), DecodedInstrucion m_instruction)  //ADD + ADD immediate
+void ADD (uint32_t  (&r_to), const uint32_t  (&r1), const uint32_t  (&r2))  //ADD + ADD immediate
 // константные ссылки. необходима для последнего аргумента чтобы работало сложение с константыой, вторая - до кучи, и потому как не нужно измеять значение второго регистра
 //при случае вызова ADDI имя функции тупо заменяется на ADD, ибо нечего 2 одинаковых функции заводить
 {	
@@ -12,13 +12,13 @@ void ADD (const Core (&o_core), DecodedInstrucion m_instruction)  //ADD + ADD im
 	writeRegister(r_to, sum);	
 };
 
-void ADDU(const Core (&o_core), DecodedInstrucion m_instruction)
+void ADDU(uint32_t  (&r_to), const uint32_t   (&r1), const uint32_t   (&r2))
 {	
 	uint32_t sum = readRegister(r1) + readRegister(r2);
 	writeRegister(r_to, sum);
 };
 
-void ADDUI(const Core (&o_core), DecodedInstrucion m_instruction)
+void ADDUI(uint32_t  (&r_to), const uint32_t   (&r1), const int32_t C)
 {	
 	uint32_t sum = readRegister(r1) + C;
 	writeRegister(r_to, sum);
@@ -27,7 +27,7 @@ void ADDUI(const Core (&o_core), DecodedInstrucion m_instruction)
 
 //commands SUB and its derivatives
 //--------------------------------------------------
-void SUB(const Core (&o_core), DecodedInstrucion m_instruction)  //sub
+void SUB(uint32_t (&r_to), const uint32_t (&r1), const uint32_t (&r2))  //sub
 // константные ссылки. необходима для последнего аргумента потому как не нужно измеять значение ни второго, ни первого регистров
 {
 	uint32_t diff = readRegister(r1) - readRegister(r2); // потом обработать случаи переполнения (?)
@@ -35,7 +35,7 @@ void SUB(const Core (&o_core), DecodedInstrucion m_instruction)  //sub
 
 };
 
-void SUBU(const Core (&o_core), DecodedInstrucion m_instruction)
+void SUBU(uint32_t (&r_to), const uint32_t (&r1), const uint32_t (&r2))
 // константные ссылки. необходима для последнего аргумента потому как не нужно измеять значение ни второго, ни первого регистров
 {
 	uint32_t diff = readRegister(r1) - readRegister(r2); // потом обработать случаи переполнения (?)
@@ -45,14 +45,14 @@ void SUBU(const Core (&o_core), DecodedInstrucion m_instruction)
 
 //commands DIV and its derivatives
 //--------------------------------------------------
-void DIV(const Core (&o_core), DecodedInstrucion m_instruction)
+void DIV(const uint32_t (&r1), const uint32_t (&r2))
 // константные ссылки. необходимы потому как не нужно изменять значение ни второго, ни первого регистров, плюс - передача по ссылке
 {
 	writeRegister( Registers::LO, readRegister(r1) / readRegister(r2) );
 	writeRegister( Registers::HI, readRegister(r1) % readRegister(r2) );
 };
 
-void DIVU(const Core (&o_core), DecodedInstrucion m_instruction)
+void DIVU(const uint32_t (&r1), const uint32_t (&r2))
 // константные ссылки. необходимы потому как не нужно изменять значение ни второго, ни первого регистров, плюс - передача по ссылке
 {
 	writeRegister( Registers::LO, (readRegister(r1) / readRegister(r2)) );
@@ -62,7 +62,7 @@ void DIVU(const Core (&o_core), DecodedInstrucion m_instruction)
 
 //commands MULT and its derivatives
 //--------------------------------------------------
-void MULT(const Core (&o_core), DecodedInstrucion m_instruction)
+void MULT(const uint32_t (&r1), const uint32_t (&r2))
 // константные ссылки. необходимы потому как не нужно изменять значение ни второго, ни первого регистров, плюс - передача по ссылке
 {
 	int64_t result = (int64_t)(readRegister(r1) * readRegister(r2));
@@ -70,7 +70,7 @@ void MULT(const Core (&o_core), DecodedInstrucion m_instruction)
 	writeRegister( Registers::HI, (result >> 32) ); // нужно ли помещать старшую часть в HI, если такого нет в reference?
 };
 
-void MULTU(const Core (&o_core), DecodedInstrucion m_instruction)
+void MULTU(const uint32_t (&r1), const uint32_t (&r2))
 // константные ссылки. необходимы потому как не нужно изменять значение ни второго, ни первого регистров, плюс - передача по ссылке
 {
 	int64_t result = (int64_t)(readRegister(r1) * readRegister(r2));
@@ -85,67 +85,67 @@ void MULTU(const Core (&o_core), DecodedInstrucion m_instruction)
 
 //commands AND and its derivatives
 //--------------------------------------------------
-void AND(const Core (&o_core), DecodedInstrucion m_instruction)
+void AND(uint32_t  (&r_to), const uint32_t  (&r1), const uint32_t  (&r2))
 {	writeRegister(r_to, (readRegister(r1) & readRegister(r2)) );	};
 
-void ANDI(const Core (&o_core), DecodedInstrucion m_instruction)
+void ANDI(uint32_t  (&r_to), const uint32_t  (&r1), const int32_t  C)
 {	writeRegister(r_to, (readRegister(r1) & C) );	};
 //--------------------------------------------------
 
 //commands OR and its derivatives
 //--------------------------------------------------
-void OR(const Core (&o_core), DecodedInstrucion m_instruction)  //Bitwise or
+void OR(uint32_t  (&r_to), const uint32_t  (&r1), const uint32_t  (&r2))  //Bitwise or
 {	writeRegister(r_to, readRegister(r1) | readRegister(r2) );		};
 
-void ORI(const Core (&o_core), DecodedInstrucion m_instruction)  // Bitwise or immediate
+void ORI(uint32_t  (&r_to), const uint32_t  (&r1), const int32_t  C)  // Bitwise or immediate
 {	writeRegister(r_to, (readRegister(r1) | C) );		};
 
-void XOR(const Core (&o_core), DecodedInstrucion m_instruction)  //Bitwise exclusive or
+void XOR(uint32_t  (&r_to), const uint32_t  (&r1), const uint32_t  (&r2))  //Bitwise exclusive or
 {	writeRegister(r_to, readRegister(r1) ^ readRegister(r2) );		};
 
-void XORI(const Core (&o_core), DecodedInstrucion m_instruction)  //Bitwise exclusive or
+void XORI(uint32_t  (&r_to), const uint32_t  (&r1), const uint32_t  C)  //Bitwise exclusive or
 {	writeRegister(r_to, (readRegister(r1) ^ C) );		};
 
-void NOR(const Core (&o_core), DecodedInstrucion m_instruction)  // Bitwise exclusive or immediate
+void NOR(uint32_t  (&r_to), const uint32_t  (&r1), const uint32_t  (&r2))  // Bitwise exclusive or immediate
 {	writeRegister(r_to, (~(r1 | r2)) );		};
 //==================================================
 
 //SHIFT OPERATIONS
 //==================================================
-void SLL(const Core (&o_core), DecodedInstrucion m_instruction) // Shift left logical
+void SLL(uint32_t  (&r_to), const uint32_t  (&r1), const int32_t C) // Shift left logical
 {	writeRegister(r_to, (readRegister(r1) << C) );		};
 
-void SRL(const Core (&o_core), DecodedInstrucion m_instruction) // Shift right logical
+void SRL(uint32_t  (&r_to), const uint32_t  (&r1), const int32_t C) // Shift right logical
 {	writeRegister(r_to, (readRegister(r1) >> C) );		};
 
-void SLLV(const Core (&o_core), DecodedInstrucion m_instruction) // Shift left logical variable
+void SLLV(uint32_t  (&r_to), const uint32_t  (&r1), const int32_t (&r2)) // Shift left logical variable
 {	writeRegister(r_to, (readRegister(r1) << readRegister(r2)) );	};
 
-void SRLV(const Core (&o_core), DecodedInstrucion m_instruction) // Shift right logical variable
+void SRLV(uint32_t  (&r_to), const uint32_t  (&r1), const int32_t (&r2)) // Shift right logical variable
 {	writeRegister(r_to, (readRegister(r1) >> readRegister(r2)) );	};
 
-void SRA(const Core (&o_core), DecodedInstrucion m_instruction) // Shift right arithmetic
+void SRA(uint32_t  (&r_to), const uint32_t  (&r1), const int32_t C) // Shift right arithmetic
 {	writeRegister(r_to, (readRegister(r1) >> C) );		}; // в чем разница с Shift right logical?!
 //==================================================
 
 //SET OPERATIONS
 //==================================================
-void SLT(const Core (&o_core), DecodedInstrucion m_instruction)  // Set on less than (signed)
+void SLT(uint32_t  (&r_to), const uint32_t  (&r1), const uint32_t  (&r2))  // Set on less than (signed)
 {	writeRegister(r_to, ( \
 						(readRegister(r1) < readRegister(r2)) ? 1 : 0 \
 						) );	};
 
-void SLTI(const Core (&o_core), DecodedInstrucion m_instruction)  // Set on less than immediate (signed)
+void SLTI(uint32_t  (&r_to), const uint32_t  (&r1), const int32_t C)  // Set on less than immediate (signed)
 {	writeRegister(r_to, ( \
 						(readRegister(r1) < C) ? 1 : 0 \
 						) );	};
 
-void SLTU(const Core (&o_core), DecodedInstrucion m_instruction)  // Set on less than (unsigned)
+void SLTU(uint32_t  (&r_to), const uint32_t  (&r1), const uint32_t  (&r2))  // Set on less than (unsigned)
 {	writeRegister(r_to, ( \
 						(readRegister(r1) < readRegister(r2)) ? 1 : 0 \
 						) );	};
 
-void SLTUI(const Core (&o_core), DecodedInstrucion m_instruction)  // Set on less than immediate (unsigned)
+void SLTUI(uint32_t  (&r_to), const uint32_t  (&r1), const int32_t C)  // Set on less than immediate (unsigned)
 {	writeRegister(r_to, ( \
 						(readRegister(r1) < C) ? 1 : 0 \
 						) );	};
@@ -153,38 +153,38 @@ void SLTUI(const Core (&o_core), DecodedInstrucion m_instruction)  // Set on les
 
 //MOVE OPERATIONS
 //==================================================
-void MFLO(const Core (&o_core), DecodedInstrucion m_instruction)  // Move from LO
+void MFLO(uint32_t  (&r_to))  // Move from LO
 {	writeRegister(r_to, readRegister(Registers::LO) );	};
 
-void MFHI(const Core (&o_core), DecodedInstrucion m_instruction)  // Move from HI
+void MFHI(uint32_t  (&r_to))  // Move from HI
 {	writeRegister(r_to, readRegister(Registers::HI) );	};
 //==================================================
 
 //LUI OPERATION
 //==================================================
-void LUI(const Core (&o_core), DecodedInstrucion m_instruction)  // load upper immediate
+void LUI(uint32_t  (&r_to), const int32_t C)  // load upper immediate
 {	writeRegister(r_to, (C << 16) );	};
 //==================================================
 
 //MEMORY OPERATIONS
 //==================================================
-void LB(const Core (&o_core), DecodedInstrucion m_instruction) //Load byte
+void LB(uint32_t  (&) r_to, const int32_t (&) r1, const uint32_t offset) //Load byte
 {	if (readByte (r1+offset, r_to) != 0 )
 	{	/*error*/	}
 };
 
-void LW(const Core (&o_core), DecodedInstrucion m_instruction) //Load byte
+void LW(uint32_t  (&r_to), const int32_t (&r1), const uint32_t offset) //Load byte
 {	if (readWord (r1+offset, r_to) != 0 )
 	{	/*error*/	}
 };
 
-void SB(const Core (&o_core), DecodedInstrucion m_instruction); // Store byte
+void SB(uint32_t  (&r_from), const uint32_t (&r1), const uint32_t offset); // Store byte
 {
 	if (writeByte(r1+offset, r_from) != 0 )
 	{	/*error*/	}
 };
 
-void SW(const Core (&o_core), DecodedInstrucion m_instruction); // Store word
+void SW(uint32_t  (&r_from), const uint32_t (&r1), const uint32_t offset); // Store word
 {
 	if (writeWord(r1+offset, r_from) != 0 )
 	{	/*error*/	}
@@ -194,128 +194,43 @@ void SW(const Core (&o_core), DecodedInstrucion m_instruction); // Store word
 //NOOP OPERATION
 //==================================================
 void NOOP() // Note: The encoding for a NOOP represents the instruction SLL $0, $0, 0 which has no side effects.
-{	o_core->advance_pc(4);	};
+{	/*advance_pc (4);*/	};
 //==================================================
 
 //SYSCALL OPERATION
 //==================================================
 void SYSCALL()
 {	
-	int tmp_reg = readRegister($v0);
-	switch(tmp_reg)
-	{
-		case 1: 
-		{	cout << readRegister(Regsters::a0) << endl;	break;	} // куда выводить - решим, переопределить поток cout придется
-		case 2: 
-		{	cout << readRegister(Regsters::f12) << endl;	break;	}
-		case 3: 
-		{	cout << readRegister(Regsters::f12) << endl;	break;	}
-		case 4: 
-		{	cout << readRegister(Regsters::a0) << ends << endl;	break;	}
-		case 5:
-		{	int num; cin >> num; writeRegister(Regsters::v0, num)); break;	} // вопрос: откуда считывать, из памяти или с... экрана?
-		case 6:
-		{	float num; cin >> num; writeRegister(Regsters::f0, num)); break;	}
-		case 7:
-		{	double num; cin >> num; writeRegister(Regsters::f0, num)); break;	}
-		case 8:
-		{	 char[readRegister(Regsters::a1)] str; cin >> str; writeRegister(Regsters::a0, str); break;	}
-		case 9:
-		{	 /* ??? */		}
-		case 10:
-		{	 /*как выходить из программы будем?! */		}
-		case 11:
-		{	 string num; cin >> num; writeRegister(Regsters::f0, num)); break;	}
-		case 12:
-		{	 char symb; cin >> symb; writeRegister(Regsters::a0, symb)); break;	}
-		case 13, 14, 15, 16:
-		{	 /* хз как делать, надо ли? */	}
-	}
-	o_core->advance_pc(4);	
+int tmp_reg = readRegister($v0); //readRegister пока не существует, исправить позже!
+switch(tmp_reg)
+{
+	case 1: 
+	{	cout << readRegister($a0) << endl;	break;	} // куда выводить - решим, переопределить поток cout придется
+	case 2: 
+	{	cout << readRegister($f12) << endl;	break;	}
+	case 3: 
+	{	cout << readRegister($f12) << endl;	break;	}
+	case 4: 
+	{	cout << readRegister($a0) << ends << endl;	break;	}
+	case 5:
+	{	int num; cin >> num; writeRegister($v0, num)); break;	} // вопрос: откуда считывать, из памяти или с... экрана?
+	case 6:
+	{	float num; cin >> num; writeRegister($f0, num)); break;	}
+	case 7:
+	{	double num; cin >> num; writeRegister($f0, num)); break;	}
+	case 8:
+	{	 char[readRegister($a1)] str; cin >> str; writeRegister($a0, str)); break;	}
+	case 9:
+	{	 /* ??? */		}
+	case 10:
+	{	 /*как выходить из программы будем?! */		}
+	case 11:
+	{	 string num; cin >> num; writeRegister($f0, num)); break;	}
+	case 12:
+	{	 char symb; cin >> symb; writeRegister($a0, symb)); break;	}
+	case 13, 14, 15, 16:
+	{	 /* хз как делать, надо ли? */	}
+}
+/*advance_pc (4);*/	
 };
-//==================================================
-
-//JUMP OPERATIONS
-//==================================================
-void J(const Core (&o_core), DecodedInstrucion m_instruction) // Jump
-{
-	o_core->m_pc = o_core->nPC; 
-	o_core->nPC = (o_core->m_pc & 0xf0000000) | (target << 2);
-}
-void JAL (const Core (&o_core), DecodedInstrucion m_instruction) // Jump and link
-{
-	writeRegister(Registers::r31, /* что за регистр 31?! */ o_core->m_pc + 8) /*(or nPC + 4)*/; 
-	o_core->m_pc = o_core->nPC; 
-	o_core->nPC = (o_core->m_pc & 0xf0000000) | (target << 2);
-}
-void JR (const Core (&o_core), DecodedInstrucion m_instruction) // Jump register
-{
-	o_core->m_pc = o_core->nPC; 
-	o_core->nPC = readRegister(m_instruction.RegisterToJump);
-}
-//==================================================
-
-//BRANCH OPERATIONS
-//==================================================
-void BEQ (const Core (&o_core), DecodedInstrucion) // Branch on equal 
-{
-	if (readRegister(r1) == readRegister(r2)) 
-		o_core->advance_pc(offset << 2);
-	else o_core->advance_pc(4);
-}
-
-void BNE (const Core (&o_core), DecodedInstrucion); // Branch on not equal
-{
-	if (readRegister(r1) != readRegister(r2)) 
-		o_core->advance_pc(offset << 2); 
-	else o_core->advance_pc(4); 
-}
-
-void BGEZ (const Core (&o_core), DecodedInstrucion); // Branch on greater than or equal to zero
-{
-	if (readRegister(r1) >= 0)
-		o_core->advance_pc(offset << 2); 
-	else o_core->advance_pc(4); 
-}
-
-void BGEZAL (const Core (&o_core), DecodedInstrucion); // Branch on greater than or equal to zero and link
-{
-	if (readRegister(r1) >= 0)
-	{	
-		writeRegister(registers::r31, o_core->m_pc + 8);	
-		o_core->advance_pc(offset << 2); 
-	}
-	else o_core->advance_pc(4); 
-}
-
-void BGTZ (const Core (&o_core), DecodedInstrucion); // Branch on greater than zero
-{
-	if (readRegister(r1) > 0)
-		o_core->advance_pc(offset << 2); 
-	else o_core->advance_pc(4); 
-}
-
-void BLEZ (const Core (&o_core), DecodedInstrucion); // Branch on less than or equal to zero
-{
-	if (readRegister(r1) <= 0)
-		o_core->advance_pc(offset << 2); 
-	else o_core->advance_pc(4); 
-}
-
-void BLTZAL (const Core (&o_core), DecodedInstrucion); // Branch on less than zero and link 
-{
-	if (readRegister(r1) < 0)
-	{	
-		writeRegister(registers::r31, o_core->m_pc + 8);	
-		o_core->advance_pc(offset << 2); 
-	}
-	else o_core->advance_pc(4); 
-}
-
-void BLTZ (const Core (&o_core), DecodedInstrucion); // Branch on less than zero
-{
-	if (readRegister(r1) < 0)
-		o_core->advance_pc(offset << 2); 
-	else o_core->advance_pc(4); 
-}
 //==================================================
--- commands/proto.h
+++ commands/proto.h
@@ -3,118 +3,118 @@
 //ARIPHMETICAL OPERATIONS
 //==================================================
 #define addi add //потому что как реализация по смыслу одна и та же из за константных ссылок в аргументах
-void ADD(uint32_t  (&), const uint32_t  (&), const uint32_t  (&));
-void ADDU(uint32_t  (&), const uint32_t  (&), const uint32_t  (&));
-void ADDUI(uint32_t  (&), const uint32_t  (&), const uint32_t);
+void ADD(const Core (&), DecodedInstrucion);
+void ADDU(const Core (&), DecodedInstrucion);
+void ADDUI(const Core (&), DecodedInstrucion);
 
-void SUB(uint32_t  (&), const uint32_t  (&), const uint32_t  (&));
-void SUBU(uint32_t  (&), const uint32_t  (&), const uint32_t  (&));
+void SUB(const Core (&), DecodedInstrucion);
+void SUBU(const Core (&), DecodedInstrucion);
 
-void DIV(const uint32_t (&), const uint32_t (&));
-void DIVU(const uint32_t (&), const uint32_t (&));
+void DIV(const Core (&), DecodedInstrucion);
+void DIVU(const Core (&), DecodedInstrucion);
 
-void MULT(const uint32_t (&), const uint32_t (&));
-void MULTU(const uint32_t (&), const uint32_t (&));
+void MULT(const Core (&), DecodedInstrucion);
+void MULTU(const Core (&), DecodedInstrucion);
 //==================================================
 
 //LOGICAL OPERATIONS
 //==================================================
-void AND(uint32_t  (&), const uint32_t  (&), const uint32_t  (&));
-void ANDI(uint32_t  (&), const uint32_t  (&), const uint32_t);
+void AND(const Core (&), DecodedInstrucion);
+void ANDI(const Core (&), DecodedInstrucion);
 
-void OR(uint32_t  (&), const uint32_t  (&), const uint32_t  (&));
-void ORI(uint32_t  (&), const uint32_t  (&), const uint32_t);
-void XOR(uint32_t  (&), const uint32_t  (&), const uint32_t  (&));
-void XORI(uint32_t  (&), const uint32_t  (&), const uint32_t  (&));
-void NOR(uint32_t  (&), const uint32_t  (&), const uint32_t  (&));
+void OR(const Core (&), DecodedInstrucion);
+void ORI(const Core (&), DecodedInstrucion);
+void XOR(const Core (&), DecodedInstrucion);
+void XORI(const Core (&), DecodedInstrucion);
+void NOR(const Core (&), DecodedInstrucion);
 
 //SHIFT OPERATIONS
 //==================================================
-void SLL(uint32_t  (&), const uint32_t  (&), const uint32_t); // Shift left logical
-void SRL(uint32_t  (&), const uint32_t  (&), const uint32_t); // Shift right logical
+void SLL(const Core (&), DecodedInstrucion); // Shift left logical
+void SRL(const Core (&), DecodedInstrucion); // Shift right logical
 
-void SLLV(uint32_t  (&), const uint32_t  (&), const uint32_t (&)); // Shift left logical variable
-void SRLV(uint32_t  (&), const uint32_t  (&), const uint32_t (&)); // Shift right logical variable
+void SLLV(const Core (&), DecodedInstrucion); // Shift left logical variable
+void SRLV(const Core (&), DecodedInstrucion); // Shift right logical variable
 
-void SRA(uint32_t  (&), const uint32_t  (&), const uint32_t); // Shift right arithmetic
+void SRA(const Core (&), DecodedInstrucion); // Shift right arithmetic
 //==================================================
 
 //SET OPERATIONS
 //==================================================
-void SLT(uint32_t  (&), const uint32_t  (&), const uint32_t  (&)); // Set on less than (signed)
-void SLTI(uint32_t  (&), const uint32_t  (&), const uint32_t); // Set on less than immediate (signed)
+void SLT(const Core (&), DecodedInstrucion); // Set on less than (signed)
+void SLTI(const Core (&), DecodedInstrucion); // Set on less than immediate (signed)
 
-void SLTU(uint32_t  (&), const uint32_t  (&), const uint32_t  (&)); // Set on less than (unsigned)
-void SLTUI(uint32_t  (&), const uint32_t  (&), const uint32_t); // Set on less than immediate (unsigned)
+void SLTU(const Core (&), DecodedInstrucion); // Set on less than (unsigned)
+void SLTUI(const Core (&), DecodedInstrucion); // Set on less than immediate (unsigned)
 //==================================================
 
 //MOVE OPERATIONS
 //==================================================
-void MFLO(uint32_t  (&));
-void MFHI(uint32_t  (&));
+void MFLO(const Core (&), DecodedInstrucion);
+void MFHI(const Core (&), DecodedInstrucion);
 //==================================================
 
 //LUI OPERATION
 //==================================================
-void LUI(uint32_t  (&), const uint32_t); //  Load upper immediate
+void LUI(const Core (&), DecodedInstrucion, const uint32_t); //  Load upper immediate
 //==================================================
 
 //MEMORY OPERATIONS
 //==================================================
-void SB(uint32_t  (&), const uint32_t (&), const uint32_t); // Store byte (NOT FINISHED YET) 
+void SB(const Core (&), DecodedInstrucion); // Store byte (NOT FINISHED YET) 
 															// MEM[$s + offset] = (0xff & $t); advance_pc (4); 
 															// sb $t, offset($s)
-void SW(uint32_t  (&), const uint32_t (&), const uint32_t); // Store word (NOT FINISHED YET) 
+void SW(const Core (&), DecodedInstrucion); // Store word (NOT FINISHED YET) 
 															// MEM[$s + offset] = $t; advance_pc (4); 
 															// sw $t, offset($s)
 
-void LB(uint32_t  (&), const uint32_t (&), const uint32_t); // Load byte (NOT FINISHED YET) 
+void LB(const Core (&), DecodedInstrucion); // Load byte (NOT FINISHED YET) 
 															// $t = MEM[$s + offset]; advance_pc (4); 
 															// lb $t, offset($s)
-void LW(uint32_t  (&), const uint32_t (&), const uint32_t); // Load word (NOT FINISHED YET) 
+void LW(const Core (&), DecodedInstrucion); // Load word (NOT FINISHED YET) 
 															// $t = MEM[$s + offset]; advance_pc (4); 
 															// lw $t, offset($s)
 //==================================================
 
 //BRANCH OPERATIONS
 //==================================================
-void BEQ (uint32_t  (&), const uint32_t (&), uint32_t); // Branch on equal (DOESNOT EXIST YET) 
+void BEQ (const Core (&), DecodedInstrucion); // Branch on equal (DOESNOT EXIST YET) 
 													   // if $s == $t advance_pc (offset << 2)); else advance_pc (4); 
 													   // beq $s, $t, offset
-void BNE (uint32_t  (&), const uint32_t (&), uint32_t); // Branch on not equal (DOESNOT EXIST YET) 
+void BNE (const Core (&), DecodedInstrucion); // Branch on not equal (DOESNOT EXIST YET) 
 													   // if $s != $t advance_pc (offset << 2)); else advance_pc (4); 
 													   // bne $s, $t, offset
 
-void BGEZ (uint32_t  (&), uint32_t); // Branch on greater than or equal to zero (DOESNOT EXIST YET) 
+void BGEZ (const Core (&), DecodedInstrucion); // Branch on greater than or equal to zero (DOESNOT EXIST YET) 
 								     // if $s >= 0 advance_pc (offset << 2)); else advance_pc (4);
 								     // bgez $s, offset
-void BGEZAL (uint32_t  (&), uint32_t); // Branch on greater than or equal to zero and link (DOESNOT EXIST YET) 
+void BGEZAL (const Core (&), DecodedInstrucion); // Branch on greater than or equal to zero and link (DOESNOT EXIST YET) 
 								   	   // if $s >= 0 $31 = PC + 8 (or nPC + 4); advance_pc (offset << 2)); else advance_pc (4);
 								 	   // bgezal $s, offset
-void BGTZ (uint32_t  (&), uint32_t); // Branch on greater than zero (DOESNOT EXIST YET) 
+void BGTZ (const Core (&), DecodedInstrucion); // Branch on greater than zero (DOESNOT EXIST YET) 
 									 // if $s > 0 advance_pc (offset << 2)); else advance_pc (4);
 									 // bgtz $s, offset
 									
-void BLEZ (uint32_t  (&), uint32_t); // Branch on less than or equal to zero (DOESNOT EXIST YET) 
-								     // if $s >= 0 advance_pc (offset << 2)); else advance_pc (4);
+void BLEZ (const Core (&), DecodedInstrucion); // Branch on less than or equal to zero (DOESNOT EXIST YET) 
+								     // if $s <= 0 advance_pc (offset << 2)); else advance_pc (4);
 								     // blez $s, offset
-void BLTZAL (uint32_t  (&), uint32_t); // Branch on less than zero and link (DOESNOT EXIST YET) 
+void BLTZAL (const Core (&), DecodedInstrucion); // Branch on less than zero and link (DOESNOT EXIST YET) 
 									   // if $s < 0 $31 = PC + 8 (or nPC + 4); advance_pc (offset << 2)); else advance_pc (4);
 									   // bltzal $s, offset
-void BLTZ (uint32_t  (&), uint32_t); // Branch on less than zero (DOESNOT EXIST YET) 
+void BLTZ (const Core (&), DecodedInstrucion); // Branch on less than zero (DOESNOT EXIST YET) 
 									 // if $s < 0 advance_pc (offset << 2)); else advance_pc (4);
 									 // bltz $s, offset
 //==================================================
 
 //JUMP OPERATIONS
 //==================================================
-void J(uint32_t); // Jump (DOESNOT EXIST YET) 
+void J(const Core (&o_core), DecodedInstrucion m_instruction); // Jump
 				  // PC = nPC; nPC = (PC & 0xf0000000) | (target << 2);
 				  // j target
-void JAL (uint32_t); // Jump and link (DOESNOT EXIST YET) 
+void JAL (const Core (&o_core), DecodedInstrucion m_instruction); // Jump and link
 					 // $31 = PC + 8 (or nPC + 4); PC = nPC; nPC = (PC & 0xf0000000) | (target << 2);
 					 // jal target
-void JR (uint32_t); // Jump register (DOESNOT EXIST YET) 
+void JR (const Core (&o_core), DecodedInstrucion m_instruction); // Jump register
 					// PC = nPC; nPC = $s;
 					// jr $s				 
 //==================================================
