0a8da4b4db4c7f19b55a4067236fd642ca49a97a
alex_mech <samik.mechanic@gmail.com>
2014-04-05 03:10:48 +0400

    -теперь используются регистры из декодированных команд - подправлены exception'ы

--- commands/commands.cpp
+++ commands/commands.cpp
@@ -7,56 +7,72 @@ using namespace std;
 void ADD (const Core (&o_core), DecodedInstrucion m_instruction)  //ADD + ADD immediate
 // константные ссылки. необходима для последнего аргумента чтобы работало сложение с константыой, вторая - до кучи, и потому как не нужно измеять значение второго регистра
 //при случае вызова ADDI имя функции тупо заменяется на ADD, ибо нечего 2 одинаковых функции заводить
-{	
-	uint32_t sum = readRegister(r1) + readRegister(r2);
-	writeRegister(r_to, sum);	
+{
+	uint32_t a = readRegister(m_instruction.rs), b = readRegister(m_instruction.rt);
+	uint32_t sum = a + b;
+	uint32_t ssd = a ^ b;
+	if ((ssd >= 0) && ((ssd ^ sum) < 0))
+		return -2; //overflow exception
+	else  
+		{ writeRegister(m_instruction.rd, sum);	return 0; }
 };
+/*overflow check using asm:
+(int a, int b) {
+  __asm__((
+     " add %0,%1\n"
+     " jo overflowed"
+     : "=0" (a): "r"(a), "r"(b): "flags"
+  ));
+  return a;
+overflowed:
+  throw integer_overflow();
+}
+*/
 
 void ADDU(const Core (&o_core), DecodedInstrucion m_instruction)
 {	
-	uint32_t sum = readRegister(r1) + readRegister(r2);
-	writeRegister(r_to, sum);
+	uint32_t sum = readRegister(m_instruction.rs) + readRegister(m_instruction.rt);
+	writeRegister(m_instruction.rd, sum);
 };
 
 void ADDUI(const Core (&o_core), DecodedInstrucion m_instruction)
 {	
-	uint32_t sum = readRegister(r1) + C;
-	writeRegister(r_to, sum);
+	uint32_t sum = readRegister(m_instruction.rs) + m_instruction.immediate;
+	writeRegister(m_instruction.rt, sum);
 };
 //--------------------------------------------------
 
 //commands SUB and its derivatives
 //--------------------------------------------------
 void SUB(const Core (&o_core), DecodedInstrucion m_instruction)  //sub
-// константные ссылки. необходима для последнего аргумента потому как не нужно измеять значение ни второго, ни первого регистров
 {
-	uint32_t diff = readRegister(r1) - readRegister(r2); // потом обработать случаи переполнения (?)
-	writeRegister(r_to, diff);
+	uint32_t a = readRegister(m_instruction.rs), b = readRegister(m_instruction.rt);
+	uint32_t diff = a - b;
+	if (diff <= a)
+		writeRegister(m_instruction.rd, diff);	
+	else return -2; //overflow exception
 
 };
 
 void SUBU(const Core (&o_core), DecodedInstrucion m_instruction)
-// константные ссылки. необходима для последнего аргумента потому как не нужно измеять значение ни второго, ни первого регистров
 {
-	uint32_t diff = readRegister(r1) - readRegister(r2); // потом обработать случаи переполнения (?)
-	writeRegister(r_to, diff);
+	uint32_t diff = readRegister(m_instruction.rs) - readRegister(m_instruction.rt);
+	writeRegister(m_instruction.rd, diff);
 };
 //--------------------------------------------------
 
 //commands DIV and its derivatives
 //--------------------------------------------------
 void DIV(const Core (&o_core), DecodedInstrucion m_instruction)
-// константные ссылки. необходимы потому как не нужно изменять значение ни второго, ни первого регистров, плюс - передача по ссылке
 {
-	writeRegister( Registers::LO, readRegister(r1) / readRegister(r2) );
-	writeRegister( Registers::HI, readRegister(r1) % readRegister(r2) );
+	writeRegister( Registers::LO, readRegister(m_instruction.rs) / readRegister(m_instruction.rt) );
+	writeRegister( Registers::HI, readRegister(m_instruction.rs) % readRegister(m_instruction.rt) );
 };
 
 void DIVU(const Core (&o_core), DecodedInstrucion m_instruction)
-// константные ссылки. необходимы потому как не нужно изменять значение ни второго, ни первого регистров, плюс - передача по ссылке
 {
-	writeRegister( Registers::LO, (readRegister(r1) / readRegister(r2)) );
-	writeRegister( Registers::HI, (readRegister(r1) % readRegister(r2)) );
+	writeRegister( Registers::LO, (readRegister(m_instruction.rs) / readRegister(m_instruction.rt)) );
+	writeRegister( Registers::HI, (readRegister(m_instruction.rs) % readRegister(m_instruction.rt)) );
 };
 //--------------------------------------------------
 
@@ -65,7 +81,7 @@ void DIVU(const Core (&o_core), DecodedInstrucion m_instruction)
 void MULT(const Core (&o_core), DecodedInstrucion m_instruction)
 // константные ссылки. необходимы потому как не нужно изменять значение ни второго, ни первого регистров, плюс - передача по ссылке
 {
-	int64_t result = (int64_t)(readRegister(r1) * readRegister(r2));
+	int64_t result = (int64_t)(readRegister(m_instruction.rs) * readRegister(m_instruction.rt));
 	writeRegister( Registers::LO, ((result << 32) >> 32) );
 	writeRegister( Registers::HI, (result >> 32) ); // нужно ли помещать старшую часть в HI, если такого нет в reference?
 };
@@ -73,7 +89,7 @@ void MULT(const Core (&o_core), DecodedInstrucion m_instruction)
 void MULTU(const Core (&o_core), DecodedInstrucion m_instruction)
 // константные ссылки. необходимы потому как не нужно изменять значение ни второго, ни первого регистров, плюс - передача по ссылке
 {
-	int64_t result = (int64_t)(readRegister(r1) * readRegister(r2));
+	int64_t result = (int64_t)(readRegister(m_instruction.rs) * readRegister(m_instruction.rt));
 	writeRegister( Registers::LO, ((result << 32) >> 32) );
 	writeRegister( Registers::HI, (result >> 32) ); // нужно ли помещать старшую часть в HI, если такого нет в reference?
 };
@@ -86,107 +102,107 @@ void MULTU(const Core (&o_core), DecodedInstrucion m_instruction)
 //commands AND and its derivatives
 //--------------------------------------------------
 void AND(const Core (&o_core), DecodedInstrucion m_instruction)
-{	writeRegister(r_to, (readRegister(r1) & readRegister(r2)) );	};
+{	writeRegister(m_instruction.rd, (readRegister(m_instruction.rs) & readRegister(m_instruction.rt)) );	};
 
 void ANDI(const Core (&o_core), DecodedInstrucion m_instruction)
-{	writeRegister(r_to, (readRegister(r1) & C) );	};
+{	writeRegister(m_instruction.rt, (readRegister(m_instruction.rs) & m_instruction.immediate) );	};
 //--------------------------------------------------
 
 //commands OR and its derivatives
 //--------------------------------------------------
 void OR(const Core (&o_core), DecodedInstrucion m_instruction)  //Bitwise or
-{	writeRegister(r_to, readRegister(r1) | readRegister(r2) );		};
+{	writeRegister(m_instruction.rd, readRegister(m_instruction.rs) | readRegister(m_instruction.rt) );		};
 
 void ORI(const Core (&o_core), DecodedInstrucion m_instruction)  // Bitwise or immediate
-{	writeRegister(r_to, (readRegister(r1) | C) );		};
+{	writeRegister(m_instruction.rt, (readRegister(m_instruction.rs) | m_instruction.immediate) );		};
 
 void XOR(const Core (&o_core), DecodedInstrucion m_instruction)  //Bitwise exclusive or
-{	writeRegister(r_to, readRegister(r1) ^ readRegister(r2) );		};
+{	writeRegister(m_instruction.rd, readRegister(m_instruction.rs) ^ readRegister(m_instruction.rt) );		};
 
 void XORI(const Core (&o_core), DecodedInstrucion m_instruction)  //Bitwise exclusive or
-{	writeRegister(r_to, (readRegister(r1) ^ C) );		};
+{	writeRegister(m_instruction.rt, (readRegister(m_instruction.rs) ^ m_instruction.immediate) );		};
 
 void NOR(const Core (&o_core), DecodedInstrucion m_instruction)  // Bitwise exclusive or immediate
-{	writeRegister(r_to, (~(r1 | r2)) );		};
+{	writeRegister(m_instruction.rd, (~(readRegister(m_instruction.rs) | readRegister(m_instruction.rt))) );		};
 //==================================================
 
 //SHIFT OPERATIONS
 //==================================================
 void SLL(const Core (&o_core), DecodedInstrucion m_instruction) // Shift left logical
-{	writeRegister(r_to, (readRegister(r1) << C) );		};
+{	writeRegister(m_instruction.rt, (readRegister(m_instruction.rs) << m_instruction.immediate) );		};
 
 void SRL(const Core (&o_core), DecodedInstrucion m_instruction) // Shift right logical
-{	writeRegister(r_to, (readRegister(r1) >> C) );		};
+{	writeRegister(m_instruction.rt, (readRegister(m_instruction.rs) >> m_instruction.immediate) );		};
 
 void SLLV(const Core (&o_core), DecodedInstrucion m_instruction) // Shift left logical variable
-{	writeRegister(r_to, (readRegister(r1) << readRegister(r2)) );	};
+{	writeRegister(m_instruction.rd, (readRegister(m_instruction.rs) << readRegister(m_instruction.rt)) );	};
 
 void SRLV(const Core (&o_core), DecodedInstrucion m_instruction) // Shift right logical variable
-{	writeRegister(r_to, (readRegister(r1) >> readRegister(r2)) );	};
+{	writeRegister(m_instruction.rd, (readRegister(m_instruction.rs) >> readRegister(m_instruction.rt)) );	};
 
 void SRA(const Core (&o_core), DecodedInstrucion m_instruction) // Shift right arithmetic
-{	writeRegister(r_to, (readRegister(r1) >> C) );		}; // в чем разница с Shift right logical?!
+{	writeRegister(m_instruction.rt, (readRegister(m_instruction.rs) >> m_instruction.immediate) );		}; // в чем разница с Shift right logical?!
 //==================================================
 
 //SET OPERATIONS
 //==================================================
 void SLT(const Core (&o_core), DecodedInstrucion m_instruction)  // Set on less than (signed)
-{	writeRegister(r_to, ( \
-						(readRegister(r1) < readRegister(r2)) ? 1 : 0 \
+{	writeRegister(m_instruction.rd, ( \
+						(readRegister(m_instruction.rs) < readRegister(m_instruction.rt)) ? 1 : 0 \
 						) );	};
 
 void SLTI(const Core (&o_core), DecodedInstrucion m_instruction)  // Set on less than immediate (signed)
-{	writeRegister(r_to, ( \
-						(readRegister(r1) < C) ? 1 : 0 \
+{	writeRegister(m_instruction.rt, ( \
+						(readRegister(m_instruction.rs) < m_instruction.immediate) ? 1 : 0 \
 						) );	};
 
 void SLTU(const Core (&o_core), DecodedInstrucion m_instruction)  // Set on less than (unsigned)
-{	writeRegister(r_to, ( \
-						(readRegister(r1) < readRegister(r2)) ? 1 : 0 \
+{	writeRegister(m_instruction.rd, ( \
+						(readRegister(m_instruction.rs) < readRegister(m_instruction.rt)) ? 1 : 0 \
 						) );	};
 
 void SLTUI(const Core (&o_core), DecodedInstrucion m_instruction)  // Set on less than immediate (unsigned)
-{	writeRegister(r_to, ( \
-						(readRegister(r1) < C) ? 1 : 0 \
+{	writeRegister(m_instruction.rt, ( \
+						(readRegister(m_instruction.rs) < m_instruction.immediate) ? 1 : 0 \
 						) );	};
 //==================================================
 
 //MOVE OPERATIONS
 //==================================================
 void MFLO(const Core (&o_core), DecodedInstrucion m_instruction)  // Move from LO
-{	writeRegister(r_to, readRegister(Registers::LO) );	};
+{	writeRegister(m_instruction.rd, readRegister(Registers::LO) );	};
 
 void MFHI(const Core (&o_core), DecodedInstrucion m_instruction)  // Move from HI
-{	writeRegister(r_to, readRegister(Registers::HI) );	};
+{	writeRegister(m_instruction.rd, readRegister(Registers::HI) );	};
 //==================================================
 
 //LUI OPERATION
 //==================================================
 void LUI(const Core (&o_core), DecodedInstrucion m_instruction)  // load upper immediate
-{	writeRegister(r_to, (C << 16) );	};
+{	writeRegister(m_instruction.rt, (m_instruction.immediate << 16) );	};
 //==================================================
 
 //MEMORY OPERATIONS
 //==================================================
 void LB(const Core (&o_core), DecodedInstrucion m_instruction) //Load byte
-{	if (readByte (r1+offset, r_to) != 0 )
+{	if (readByte (readRegister(m_instruction.rs)+offset, m_instruction.rt) != 0 )
 	{	/*error*/	}
 };
 
-void LW(const Core (&o_core), DecodedInstrucion m_instruction) //Load byte
-{	if (readWord (r1+offset, r_to) != 0 )
+void LW(const Core (&o_core), DecodedInstrucion m_instruction) //Load word
+{	if (readWord (readRegister(m_instruction.rs)+m_instruction.immediate)!= 0 ); /*(оттуда ли брать Offset?)*/, m_instruction.rt) != 0 )
 	{	/*error*/	}
 };
 
 void SB(const Core (&o_core), DecodedInstrucion m_instruction); // Store byte
 {
-	if (writeByte(r1+offset, r_from) != 0 )
+	if (writeByte(readRegister(m_instruction.rs)+m_instruction.immediate, (0xff & readRegister(m_instruction.rt))) != 0 )
 	{	/*error*/	}
 };
 
 void SW(const Core (&o_core), DecodedInstrucion m_instruction); // Store word
 {
-	if (writeWord(r1+offset, r_from) != 0 )
+	if (writeWord(readRegister(m_instruction.rs)+offset, (0xff & readRegister(m_instruction.rt))) != 0 )
 	{	/*error*/	}
 };
 //==================================================
@@ -251,7 +267,7 @@ void JAL (const Core (&o_core), DecodedInstrucion m_instruction) // Jump and lin
 void JR (const Core (&o_core), DecodedInstrucion m_instruction) // Jump register
 {
 	o_core->m_pc = o_core->nPC; 
-	o_core->nPC = readRegister(m_instruction.RegisterToJump);
+	o_core->nPC = readRegister(m_instruction.address);
 }
 //==================================================
 
@@ -259,63 +275,63 @@ void JR (const Core (&o_core), DecodedInstrucion m_instruction) // Jump register
 //==================================================
 void BEQ (const Core (&o_core), DecodedInstrucion) // Branch on equal 
 {
-	if (readRegister(r1) == readRegister(r2)) 
-		o_core->advance_pc(offset << 2);
+	if (readRegister(m_instruction.rs) == readRegister(m_instruction.rt)) 
+		o_core->advance_pc(m_instruction.immediate << 2);
 	else o_core->advance_pc(4);
 }
 
 void BNE (const Core (&o_core), DecodedInstrucion); // Branch on not equal
 {
-	if (readRegister(r1) != readRegister(r2)) 
-		o_core->advance_pc(offset << 2); 
+	if (readRegister(m_instruction.rs) != readRegister(m_instruction.rt)) 
+		o_core->advance_pc(m_instruction.immediate << 2); 
 	else o_core->advance_pc(4); 
 }
 
 void BGEZ (const Core (&o_core), DecodedInstrucion); // Branch on greater than or equal to zero
 {
-	if (readRegister(r1) >= 0)
+	if (readRegister(m_instruction.rs) >= 0)
 		o_core->advance_pc(offset << 2); 
 	else o_core->advance_pc(4); 
 }
 
 void BGEZAL (const Core (&o_core), DecodedInstrucion); // Branch on greater than or equal to zero and link
 {
-	if (readRegister(r1) >= 0)
+	if (readRegister(m_instruction.rs) >= 0)
 	{	
 		writeRegister(registers::r31, o_core->m_pc + 8);	
-		o_core->advance_pc(offset << 2); 
+		o_core->advance_pc(m_instruction.immediate << 2); 
 	}
 	else o_core->advance_pc(4); 
 }
 
 void BGTZ (const Core (&o_core), DecodedInstrucion); // Branch on greater than zero
 {
-	if (readRegister(r1) > 0)
-		o_core->advance_pc(offset << 2); 
+	if (readRegister(m_instruction.rs) > 0)
+		o_core->advance_pc(m_instruction.immediate << 2); 
 	else o_core->advance_pc(4); 
 }
 
 void BLEZ (const Core (&o_core), DecodedInstrucion); // Branch on less than or equal to zero
 {
-	if (readRegister(r1) <= 0)
-		o_core->advance_pc(offset << 2); 
+	if (readRegister(m_instruction.rs) <= 0)
+		o_core->advance_pc(m_instruction.immediate << 2); 
 	else o_core->advance_pc(4); 
 }
 
 void BLTZAL (const Core (&o_core), DecodedInstrucion); // Branch on less than zero and link 
 {
-	if (readRegister(r1) < 0)
+	if (readRegister(m_instruction.rs) < 0)
 	{	
 		writeRegister(registers::r31, o_core->m_pc + 8);	
-		o_core->advance_pc(offset << 2); 
+		o_core->advance_pc(m_instruction.immediate << 2); 
 	}
 	else o_core->advance_pc(4); 
 }
 
 void BLTZ (const Core (&o_core), DecodedInstrucion); // Branch on less than zero
 {
-	if (readRegister(r1) < 0)
-		o_core->advance_pc(offset << 2); 
+	if (readRegister(m_instruction.rs) < 0)
+		o_core->advance_pc(m_instruction.immediate << 2); 
 	else o_core->advance_pc(4); 
 }
 //==================================================
